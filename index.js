87f65ba90df6fa8ee41b5d04ed94f07b6823db5c
Douglas Jorge Vizzacchi <douglas@vizzacchi.com.br>
2022-09-17 20:37:28 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express    = require("express");
+const morgan     = require("morgan");
+const cors       = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+
+let db = [
+    {'1': { Nome: 'Cliente 1', Idade: '20'} },
+    {'2': { Nome: 'Cliente 2', Idade: '20'} },
+    {'3': { Nome: 'Cliente 3', Idade: '20'} }
+];
+
+app.get('/', (req, res) => {
+    return res.json(db);
+});
+
+
+
+
+
+app.listen(21262,() => { 
+    console.log(`Express new started at http://localhost:21262`)
+})
